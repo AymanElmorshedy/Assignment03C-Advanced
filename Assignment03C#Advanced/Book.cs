ff6854b134db1893e24b3dbaf642a04a3aa25a75
ayman <aymanehab2002@gmail.com>
2025-02-05 16:22:55 +0200

    Q01

---
+++ Assignment03C#Advanced/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03C_Advanced
+{
+    internal class Book
+    {
+
+        
+        
+        public string ISPN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        
+
+        
+        public Book(string iSPN, string title, string[] authors, DateTime publicationDate, decimal price)
+        {
+            ISPN = iSPN;
+            Title = title;
+            Authors = authors;
+            this.PublicationDate = publicationDate;
+            Price = price;
+        }
+        
+
+        public override string ToString() => $"ISBN:{ISPN}, Title:{Title}, Authors:{Authors}, DataTimePublicationDate:{PublicationDate}, Price:{Price}";
+
+      
+        
+    }
+}
---
+++ Assignment03C#Advanced/BookFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03C_Advanced
+{
+    internal static class BookFunctions
+    {
+        public static string GetTitle(Book book) =>book.Title;
+
+        public static string[] GetAuthors(Book book)=>book.Authors;
+
+        public static string GetPrice(Book book)=>book.Price.ToString();
+    }
+}
